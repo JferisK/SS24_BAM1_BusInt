d3d909f64537cf399e3c8e192210566716d46ba9
feris <52134775+JferisK@users.noreply.github.com>
2024-05-15 23:33:05 +0200

    update

--- loan-broker/src/main/java/de/jakob_kroemer/LoanBrokerApplication.java
+++ loan-broker/src/main/java/de/jakob_kroemer/LoanBrokerApplication.java
@@ -1,12 +1,10 @@
 package de.jakob_kroemer;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ImportResource("classpath:META-INF/spring/integration/loan-broker-config.xml")
@@ -15,8 +13,4 @@ public class LoanBrokerApplication {
     public static void main(String[] args) {
         SpringApplication.run(LoanBrokerApplication.class, args);
     }
-    
-	/*
-	 * @Bean public RestTemplate restTemplate() { return new RestTemplate(); }
-	 */
 }
